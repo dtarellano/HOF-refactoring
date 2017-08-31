e94cf47e92a4bd7b63ddde20ce860a3526c9116a
David Arellano <dtarellano1@gmail.com>
2017-08-30 19:31:16 -0700

    added removeElement

--- HOFrefactoring.js
+++ HOFrefactoring.js
@@ -62,3 +62,15 @@ function getElementsGreaterThan10AtProperty(obj, key) {
   }
   return [];
 }
+
+
+// Write a function called "removeElement".
+//
+// Given an array of elements, and a "discarder" parameter, "removeElement" returns an array containing the items in the given array that do not match the "discarder" parameter.
+
+
+function removeElement(array, discarder) {
+  return array.filter(function(value, i) {
+    return value !== discarder;
+  });
+}
